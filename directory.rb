2af6b305b5b23f2a711d402b39df2d0cdde74b38
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-05 16:30:39 +0000

    Added student count and comments, further improvement needed.

--- directory.rb
+++ directory.rb
@@ -1,5 +1,9 @@
+#Variable to track student number
+students = 15
+#Title and head divider
 puts "Villians of this here Villain Academy, assembled!"
 puts "~|------------------------------------------------|~"
+#Students!
 puts "Dr. Hannibal Lecter"
 puts "Darth Vader"
 puts "Ludo Kressh"
@@ -15,4 +19,7 @@ puts "Hush"
 puts "Joffrey Baratheon"
 puts "Daenerys Targaryen (I mean c'mon now, have you seen?)"
 puts "Norman Bates"
+#And now to state the number of students!
+puts "Overall, we have #{students} dastardly students!"
+#Footer, for style purposes!
 puts "~|------------------------------------------------|~"
