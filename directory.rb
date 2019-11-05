f619792229f0fe67e53dee07194548e5bf609f0e
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-05 16:43:20 +0000

    Array-ified the student list for ease of management.

--- directory.rb
+++ directory.rb
@@ -1,25 +1,27 @@
-#Variable to track student number
-students = 15
+#Our list of villainous students, of studious villains!
+villains = [ "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Ludo Kressh",
+  "Naga Sadow",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex Delarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Hush",
+  "Joffrey Baratheon",
+  "Daenerys Targaryen",
+  "Norman Bates"
+]
 #Title and head divider
 puts "Villians of this here Villain Academy, assembled!"
 puts "~|------------------------------------------------|~"
-#Students!
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Ludo Kressh"
-puts "Naga Sadow"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Hush"
-puts "Joffrey Baratheon"
-puts "Daenerys Targaryen (I mean c'mon now, have you seen?)"
-puts "Norman Bates"
-#And now to state the number of students!
-puts "Overall, we have #{students} dastardly students!"
+#Presenting our students!
+villains.each { |v| puts v}
+#And now to state the number of students,
+#which is the length of the villains array!
+puts "Overall, we have #{villains.length} dastardly students!"
 #Footer, for style purposes!
 puts "~|------------------------------------------------|~"
