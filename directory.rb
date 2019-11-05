37f2b9fb98e948d33e4fcb7bdda74d9825d12733
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-05 17:23:02 +0000

    Used hashes to add cohort info

--- directory.rb
+++ directory.rb
@@ -1,33 +1,36 @@
 #Our list of villainous students, of studious villains!
-villains = [ "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Ludo Kressh",
-  "Naga Sadow",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex Delarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Hush",
-  "Joffrey Baratheon",
-  "Daenerys Targaryen",
-  "Norman Bates"
-]
+villains = [
+  {name: "Dr. Hannibal Lecter", month: :november},
+  {name: "Darth Vader", month: :november},
+  {name: "Ludo Kressh", month: :november},
+  {name: "Naga Sadow", month: :november},
+  {name: "Nurse Ratched", month: :november},
+  {name: "Michael Corleone", month: :november},
+  {name: "Alex Delarge", month: :november},
+  {name: "The Wicked Witch of the West", month: :november},
+  {name: "Terminator", month: :november},
+  {name: "Freddy Krueger", month: :november},
+  {name: "The Joker", month: :november},
+  {name: "Hush", month: :november},
+  {name: "Joffrey Baratheon", month: :november},
+  {name: "Daenerys Targaryen", month: :november},
+  {name: "Norman Bates", month: :november} ]
 #Title and head divider
 def print_header
-  puts "Villians of this here Villain Academy, assembled!"
+  puts "Villians of the Villain Academy, assemble!"
   puts "~|------------------------------------------------|~"
 end
 #Presenting our students!
 
 def print_list(ls)
-  ls.each { |v| puts v }
+  ls.each do |vl|
+    puts "#{vl[:name]} (#{vl[:month]} cohort)"
+  end
 end
 
 #Footer, for style purposes! Array length, for student count!
 def footer(ls)
+  puts " "
   puts "Overall, we have #{ls.length} dastardly students!"
   puts "~|------------------------------------------------|~"
 end
