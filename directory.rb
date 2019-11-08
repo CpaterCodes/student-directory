5989df2e90d28d518eb385d2c8ad92853702ae78
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-08 15:28:49 +0000

    Added file saving and plurality tracking

--- directory.rb
+++ directory.rb
@@ -1,20 +1,6 @@
 #Our list of villainous students, of studious villains!
-villains = [
-  {name: "Dr. Hannibal Lecter", month: :november},
-  {name: "Darth Vader", month: :november},
-  {name: "Ludo Kressh", month: :november},
-  {name: "Naga Sadow", month: :november},
-  {name: "Nurse Ratched", month: :november},
-  {name: "Michael Corleone", month: :november},
-  {name: "Alex Delarge", month: :november},
-  {name: "The Wicked Witch of the West", month: :november},
-  {name: "Terminator", month: :november},
-  {name: "Freddy Krueger", month: :november},
-  {name: "The Joker", month: :november},
-  {name: "Hush", month: :november},
-  {name: "Joffrey Baratheon", month: :november},
-  {name: "Daenerys Targaryen", month: :november},
-  {name: "Norman Bates", month: :november} ]
+@villains = []
+
 
 #Title and head divider
 def print_header
@@ -23,52 +9,75 @@ def print_header
 end
 #Presenting our students!
 
-def print_list(list)
-  list.each_with_index do |villain,number|
-    puts "#{number}:#{villain[:name]} (#{villain[:month]} cohort)" unless list.empty?
+def print_list
+  @villains.each_with_index do |vil,n|
+    puts "#{n}:#{vil[:name]} (#{vil[:month]} cohort)" unless @villains.empty?
   end
-    puts "Nothing to see here :/" if list.empty?
+    puts "Nothing to see here :/" if @villains.empty?
+end
+
+#A bit of code for later use, to ensure proper singular vs plural use
+def plural
+  return @villains.length > 1 ? 's' : ''
 end
 
 #Footer, for style purposes! Array length, for student count!
-def footer(list)
-  unless list.empty?
+def footer
+  unless @villains.empty?
     puts " "
-    puts "Overall, we have #{list.length} dastardly students!"
+    puts "Overall, we have #{@villains.length} dastardly student#{plural}!"
   end
   puts "~|------------------------------------------------|~"
 end
 
 #But what if we want to add students?
-def add_student(list)
+def add_student
   puts "Enter all new Villains you require."
-  puts "Press return when you are finished."
+  puts "Press return without entering another name to finish."
   #Add a name if desired, and then keep adding names if desired
   name = gets.chomp
   until name.empty?
-    list << {name: name, month: :november}
+    @villains << {name: name, month: :november}
     #Update the user, and ask for the next name. Hit return to stop.
-    puts "#{name} added, now there are #{list.length} students total!"
+    puts "#{name} added, we have #{@villains.length} student#{plural} total!"
     puts "Type a name to add another student or press return to stop."
     name = gets.chomp
   end
 end
 
-def menu_interactive(list)
+def save_students
+  directory = File.open("Villains.csv", "w")
+  @villains.each do |vil|
+    villain_data = [vil[:name], vil[:month]]
+    villain_row = villain_data.join(", ")
+    directory.puts villain_row
+  end
+  directory.close
+end
+
+def interactive_menu
+  #Introduce the program and menu
   puts "Welcome to the directory!"
+  #Ask user what they wish to do, perform it, and ask again
+  #until 'exit' is chosen
   loop do
     puts "What do you wish to do?"
     puts "----------------------"
-    puts "Read | Add students | Exit"
+    puts "1:List students | 2:Add students | 3:Save changes | 9:Exit"
     choice = gets.chomp
     case choice
-    when "Read"
-    print_header
-    print_list(list)
-    footer(list)
-    when "Add students"
-    add_student(list)
-    when "Exit"
+    #Print the directory if user asks to list students
+    when "1"
+      print_header
+      print_list
+      footer
+    #Run the procedure for adding students if user asks to add students
+    when "2"
+      add_student
+    when "3"
+      save_students
+    #Exit the program by breaking the loop if users asks to exit
+    when "9"
       break
     else
       nil
@@ -76,4 +85,4 @@ def menu_interactive(list)
   end
 end
 
-menu_interactive(villains)
+interactive_menu
